3751a0d4b8db827f2b55eb7dd825935c1fec2e84
bischoff-m <bischoff_m@aim.com>
2024-07-28 01:56:21 +0200

    Calc cell color as 2-color gradient of when it was last active

--- src/main.py
+++ src/main.py
@@ -1,28 +1,59 @@
-from typing import List
+from typing import Callable, List, Optional
 import pygame
 import sys
 import numpy as np
 from ui_elements import Button
 
+
+class Gradient:
+    def __init__(
+        self,
+        primary: pygame.Color,
+        secondary: pygame.Color,
+        steps: int,
+        ease_func: Optional[Callable[[float], float]] = None,
+    ):
+        self.steps = steps
+        # Calculate immediate color steps
+        # 0 -> primary, steps -> secondary
+        self.colors: List[pygame.Color] = []
+        for i in range(steps):
+            val = i / (steps - 1)
+            if ease_func:
+                val = ease_func(val)
+            self.colors.append(primary.lerp(secondary, val))
+
+    def get(self, step: int) -> pygame.Color:
+        if step < 0 or step >= self.steps:
+            raise ValueError("Step out of bounds")
+        return self.colors[step]
+
+
 # Constants
-BLACK = pygame.Color("#2f3061")
-WHITE = pygame.Color("#dfdfdf")
+PRIMARY_COLOR = pygame.Color("#d49d6a")
+SECONDAY_COLOR = pygame.Color("#2f4073")
 GRID_HEIGHT = 750
 GRID_WIDTH = 800
 FOOTER_HEIGHT = 36
 BLOCK_SIZE = 10
-state = np.zeros((GRID_WIDTH // BLOCK_SIZE, GRID_HEIGHT // BLOCK_SIZE))
-prev_states = np.zeros((3, state.shape[0], state.shape[1]))
+gradient = Gradient(PRIMARY_COLOR, SECONDAY_COLOR, 100, lambda x: x**0.5)
 ui_elements: List[Button] = []
-is_paused = True
+is_paused = False
+
+# State of the grid
+# 0 = currently active
+# 1 = was active 1 step ago
+# n = inactive
+# Game of Life rules are applied for == 0 (active) and > 0 (inactive)
+state = np.zeros((GRID_WIDTH // BLOCK_SIZE, GRID_HEIGHT // BLOCK_SIZE), dtype=int)
 
 
 def main():
-    global SCREEN, CLOCK, state, is_paused
+    global SCREEN, CLOCK
     pygame.init()
     SCREEN = pygame.display.set_mode((GRID_WIDTH, GRID_HEIGHT + FOOTER_HEIGHT))
     CLOCK = pygame.time.Clock()
-    SCREEN.fill(BLACK)
+    SCREEN.fill(SECONDAY_COLOR)
 
     # Initialize state
     init_state_random()
@@ -56,51 +87,50 @@ def main():
 def init_state_random():
     for x, y in np.ndindex(state.shape):
         state[x][y] = np.random.randint(0, 2)
+    # Set all inactive cells to max value
+    state[state == 1] = gradient.steps - 1
 
 
 def update_state():
-    # Game of Life rules
+    global state
+    # For new_state == 1 <-> active, == 0 <-> inactive
     new_state = np.zeros_like(state)
+
+    # Game of Life rules
     for x, y in np.ndindex(state.shape):
+        # Count neighbors
         neighbors = 0
         for dx, dy in np.ndindex((3, 3)):
             if dx == 1 and dy == 1:
                 continue
             new_x = x + dx - 1
             new_y = y + dy - 1
-            if 0 <= new_x < state.shape[0] and 0 <= new_y < state.shape[1]:
-                neighbors += state[new_x][new_y]
+            if (
+                0 <= new_x < state.shape[0]
+                and 0 <= new_y < state.shape[1]
+                and state[new_x][new_y] == 0
+            ):
+                neighbors += 1
 
-        if state[x][y] == 1:
-            if neighbors < 2 or neighbors > 3:
-                new_state[x][y] = 0
-            else:
+        # Apply rules
+        if state[x][y] == 0:
+            if neighbors == 2 or neighbors == 3:
                 new_state[x][y] = 1
         else:
             if neighbors == 3:
                 new_state[x][y] = 1
 
-    # Update state
-    prev_states[2] = prev_states[1]
-    prev_states[1] = prev_states[0]
-    prev_states[0] = state.copy()
-    state[:] = new_state
+    # Shift inactive cells
+    state += 1
+    # Set cells to 0 that are active now
+    state[new_state == 1] = 0
+    # Clip to max value
+    state = np.clip(state, 0, gradient.steps - 1)
 
 
 def draw_grid():
     for x, y in np.ndindex(state.shape):
-        # Brightness ranges from 0 to 1
-        brightness = state[x][y]
-        brightness += 0.5 * prev_states[0][x][y]
-        brightness += 0.25 * prev_states[1][x][y]
-        brightness += 0.125 * prev_states[2][x][y]
-        brightness = min(1, brightness)
-        # Use darkness to scale color
-        color = pygame.Color(
-            int(WHITE.r * brightness),
-            int(WHITE.g * brightness),
-            int(WHITE.b * brightness),
-        )
+        color = gradient.get(state[x][y])
         rect = pygame.Rect(x * BLOCK_SIZE, y * BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE)
         pygame.draw.rect(SCREEN, color, rect)
 
